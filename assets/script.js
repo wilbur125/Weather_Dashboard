398a5f352109deca921450ce9594f1f672452278
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-10 02:04:35 -0800

    Added code to display weather over future five day forcast

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@
  const todayHumidity = document.getElementById('today-humidity');
  const todayWindspeed = document.getElementById('today-windspeed');
  const todayUVI = document.getElementById('today-uvi');
+ const fiveDayRow = document.getElementById('five-day');
 
 let city;
 
@@ -57,8 +58,92 @@ function displayCurrentWeather() {
 }
 
 function fiveDayForecast() {
-    //ajax for 5 day forcast
-    //for each to create a card and display data for city input
+    let city = cityInput.value.trim();
+
+    let fiveDayQuery = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&lang=en&appid=0a0e18cfc10038e9c1b25028eac7bb5c";
+
+    $.ajax({
+        url: fiveDayQuery,
+        method: "GET"
+    }).then(function(weatherFive) {
+        console.log("five",weatherFive);
+
+        //created new array of objects to store api data for 5 day forcast at noon
+        let fiveDayResults = [
+            {
+                date: weatherFive.list[7].dt_txt,
+                src: "http://openweathermap.org/img/wn/" + weatherFive.list[7].weather[0].icon + "@2x.png",
+                alt: weatherFive.list[7].weather[0].description,
+                temp: weatherFive.list[7].main.temp,
+                humidity: weatherFive.list[7].main.humidity     
+            },
+            {
+                date: weatherFive.list[15].dt_txt,
+                src: "http://openweathermap.org/img/wn/" + weatherFive.list[15].weather[0].icon + "@2x.png",
+                alt: weatherFive.list[15].description,
+                temp: weatherFive.list[15].main.temp,
+                humidity: weatherFive.list[15].main.humidity  
+            },
+            {
+                date: weatherFive.list[23].dt_txt,
+                src: "http://openweathermap.org/img/wn/" + weatherFive.list[23].weather[0].icon + "@2x.png",
+                alt: weatherFive.list[23].description,
+                temp: weatherFive.list[23].main.temp,
+                humidity: weatherFive.list[23].main.humidity  
+            },
+            {
+                date: weatherFive.list[31].dt_txt,
+                src: "http://openweathermap.org/img/wn/" + weatherFive.list[31].weather[0].icon + "@2x.png",
+                alt: weatherFive.list[31].description,
+                temp: weatherFive.list[31].main.temp,
+                humidity: weatherFive.list[31].main.humidity  
+            },
+            {
+                date: weatherFive.list[39].dt_txt,
+                src: "http://openweathermap.org/img/wn/" + weatherFive.list[39].weather[0].icon + "@2x.png",
+                alt: weatherFive.list[39].description,
+                temp: weatherFive.list[39].main.temp,
+                humidity: weatherFive.list[39].main.humidity  
+            }
+        ];
+
+        console.log('wut', fiveDayResults);
+
+        for (i = 0; i < fiveDayResults.length; i++) {
+            const fiveCol = document.createElement('div');
+            fiveCol.classList.add('col-sm-2');
+            fiveDayRow.appendChild(fiveCol);
+
+            const fiveCard = document.createElement('div');
+            fiveCard.classList.add('card');
+            fiveCol.appendChild(fiveCard);
+            
+            const fiveCardBody = document.createElement('div');
+            fiveCardBody.classList.add('card-body');
+            fiveCard.appendChild(fiveCardBody);
+
+            const fiveHeading = document.createElement('h7');
+            fiveHeading.innerText = fiveDayResults[i].date;
+            fiveCardBody.appendChild(fiveHeading);
+            
+            const fiveIcon = document.createElement('img');
+            const attSRC = document.createAttribute('src');
+            attSRC.value = fiveDayResults[i].src;
+            fiveIcon.setAttributeNode(attSRC);
+            const attALT = document.createAttribute('alt');
+            attALT.value = fiveDayResults[i].alt;
+            fiveIcon.setAttributeNode(attALT);
+            fiveCardBody.appendChild(fiveIcon);
+            
+            const fiveTemp = document.createElement('div');
+            fiveTemp.innerText = fiveDayResults[i].temp;
+            fiveCardBody.appendChild(fiveTemp);
+            
+            const fiveHumidity = document.createElement('div');
+            fiveHumidity.innerText = fiveDayResults[i].humidity;
+            fiveCardBody.appendChild(fiveHumidity);
+        };
+    })
 }
 
 
@@ -73,5 +158,6 @@ function pastCities() {
 
         
 searchButton.addEventListener('click', displayCurrentWeather);
-searchButton.addEventListener('click', pastCities);
 
+searchButton.addEventListener('click', fiveDayForecast);
+searchButton.addEventListener('click', pastCities);
